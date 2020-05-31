441a7627d5871f7384fa2d73b09899c98891153d
Chase <cdhaymond@gmail.com>
2020-05-31 01:27:03 -0600

    added praw credential authenticating code

--- reddit_wallpaper_getter.py
+++ reddit_wallpaper_getter.py
@@ -1,2 +1,13 @@
-print("hello world, again")
+import praw
 
+reddit = praw.Reddit(client_id="",
+                     client_secret="",
+                     password="",
+                     user_agent="",
+                     username="")
+
+print(reddit.user.me())
+
+reddit.read_only = True
+
+print(reddit.user.me())
